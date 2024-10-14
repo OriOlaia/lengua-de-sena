48f69a91ad126bcef02b3d00cb2e8bce57fa347c
Oriana Olaia <68045578+OriOlaia@users.noreply.github.com>
2024-10-14 10:43:57 -0300

    Add files via upload

--- src/components/Princip.jsx
+++ src/components/Princip.jsx
@@ -2,7 +2,19 @@ import "./Princip.css";
 
 export const Princip = () => {
   return (
-    <div>
+    <div className="principal">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quis
+        quod necessitatibus molestiae ad veritatis sunt laboriosam voluptas,
+        suscipit dolore, rerum, voluptatibus minus! Consequatur tempora vero
+        repellendus libero quaerat quidem.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quis
+        quod necessitatibus molestiae ad veritatis sunt laboriosam voluptas,
+        suscipit dolore, rerum, voluptatibus minus! Consequatur tempora vero
+        repellendus libero quaerat quidem.
+      </p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident quis
         quod necessitatibus molestiae ad veritatis sunt laboriosam voluptas,
